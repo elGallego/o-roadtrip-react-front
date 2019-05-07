7f8b072e4b235e90583a9bb85c63ad80db81da66
elGallego <cheelcelta@gmail.com>
2019-05-07 11:57:25 +0200

    install redux and create store

---
+++ src/store/reducer.js
@@ -0,0 +1,8 @@
+// le plus petit reducer que l'on peut avoir
+// le reducer est une fonction qui prend en paramètre le state et selon l'action et nous renvoi l'etat du state.
+// reducer est une fonction qui renvoi un objet
+const reducer = (state= {}, action= {}) => {
+    return state;
+};
+
+export default reducer; 
