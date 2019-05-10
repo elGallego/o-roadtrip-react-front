b84b141832f25d974c74cf99302ee28c28bb6729
elGallego <cheelcelta@gmail.com>
2019-05-10 20:58:33 +0200

    test API avec Alexis

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
 import Map from '../Map';
 import WrapFav from '../WrapFav';
 
 import './index.scss';
 
-const App = () => (
-    <div id="App">
-        <Map />
-        <WrapFav />
-    </div>
-);
+const App = () => {
+    useEffect(()=> {
+        axios.get('https://places.api.here.com/places/v1/discover/search?app_id=2JhxBimvjIWjT0b2gg2w&app_code=Cq-FoqMWZumjbZmgPczzzw&at=52.531,13.3843&q=Brandenburg+Gate&pretty')
+            .then(console.log)
+            .catch(console.log);
+    });
+    return (
+        <div id="App">
+            <Map />
+            <WrapFav />
+        </div>
+    );
+};      
 
 export default App;
--- src/components/WrapFav/index.js
+++ src/components/WrapFav/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import FavDetails from '../FavDetails';
 import FavClose from '../FavClose';
+import CardRequest from '../CardRequest';
 
 import './index.scss';
 
@@ -10,6 +11,8 @@ const WrapFav = () => (
     <h2>Vos favoris</h2>
     <FavDetails />
     <FavClose />
+    <h2>Résultats de votre recherche</h2>
+    <CardRequest />
   </div>
   );
 
